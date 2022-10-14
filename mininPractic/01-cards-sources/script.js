553078f2fa83c58aa6842028fd6a8029e07b6892
Vladislav <vvrych3@gmail.com>
2022-10-15 03:35:36 +0600

    feat: add plagin for slider

--- mininPractic/01-cards-sources/script.js
+++ mininPractic/01-cards-sources/script.js
@@ -1,16 +1,23 @@
 "use strict";
-const slides = document.querySelectorAll('.slide');
+function sliderPlugin(acitveElement = 0) {
+    const slides = document.querySelectorAll('.slide');
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    });
+    slides[acitveElement].classList.add('active');
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+    }
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        });
+    }
 }
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
-    });
-}
+sliderPlugin(2);
+
 
